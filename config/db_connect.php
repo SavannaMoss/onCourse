2df38af5f6edf47655a7a9fb930e936f572cfadf
Savanna Moss <60207230+SavannaMoss@users.noreply.github.com>
2020-11-03 12:39:27 -0500

    Update db_connect.php

--- config/db_connect.php
+++ config/db_connect.php
@@ -3,8 +3,6 @@
 	// connect to the database
 	$conn = mysqli_connect('localhost', 'root', 'root', 'oncourse');
 
-	// $conn = mysqli_connect('localhost', 'root', 'testing123', 'oncourse'); // for AWS
-
 	// check connection
 	if(!$conn){
 		echo 'Connection error: '. mysqli_connect_error();
